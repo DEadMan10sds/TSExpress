571823c54f9927739a7d6418ca06cdf2a279e3df
DEadMan10sds <alejandroadan.sds@gmail.com>
2024-03-29 18:04:35 -0600

    ADD: Middleware validaton for articles

--- src/routes/article.ts
+++ src/routes/article.ts
@@ -1,10 +1,86 @@
 import { Router } from "express";
 import { articleController } from "../controllers/article";
+import { validateFields } from "../middlewares/validateFields";
+import { check } from "express-validator";
+import { validateJWT } from "../middlewares/validateJWT";
 
 export const articleRouter = Router();
 
-articleRouter.get("/:id?", articleController.getArticles);
-articleRouter.post("/new", articleController.createArticle);
-articleRouter.put("/update/:id", articleController.createArticle);
-articleRouter.put("/soft/:id", articleController.softDeleteArticle);
-articleRouter.delete("/delete/:id", articleController.deleteArticle);
+articleRouter.get(
+  "/:id?",
+  [
+    check("id")
+      .optional()
+      .isMongoId()
+      .withMessage("El id no es un id de mongo válido"),
+    validateFields,
+  ],
+  articleController.getArticles
+);
+
+articleRouter.post(
+  "/new",
+  [
+    check("title")
+      .notEmpty()
+      .withMessage("El título es obligatorio")
+      .isString()
+      .withMessage("El título tiene que ser un string"),
+    check("content")
+      .notEmpty()
+      .withMessage("El contenido del artículo es obligatorio")
+      .isString()
+      .withMessage("El contenido tiene que ser texto"),
+    check("imgURL")
+      .optional()
+      .isURL()
+      .withMessage("La imagen tiene que ser una URL"),
+    validateJWT,
+    validateFields,
+  ],
+  articleController.createArticle
+);
+
+articleRouter.put(
+  "/update/:id",
+  [
+    check("id").isMongoId().withMessage("El id no es un id de mongo válido"),
+    check("title")
+      .optional()
+      .withMessage("El título es obligatorio")
+      .isString()
+      .withMessage("El título tiene que ser un string"),
+    check("content")
+      .optional()
+      .withMessage("El contenido del artículo es obligatorio")
+      .isString()
+      .withMessage("El contenido tiene que ser texto"),
+    check("imgURL")
+      .optional()
+      .isURL()
+      .withMessage("La imagen tiene que ser una URL"),
+    validateJWT,
+    validateFields,
+  ],
+  articleController.createArticle
+);
+
+articleRouter.put(
+  "/soft/:id",
+  [
+    check("id").isMongoId().withMessage("El id no es un id de mongo válido"),
+    validateJWT,
+    validateFields,
+  ],
+  articleController.softDeleteArticle
+);
+
+articleRouter.delete(
+  "/delete/:id",
+  [
+    check("id").isMongoId().withMessage("El id no es un id de mongo válido"),
+    validateJWT,
+    validateFields,
+  ],
+  articleController.deleteArticle
+);
